e7b70394269d11b0689b6732ec9bb7039eb8d21f
abdullah1107 <abdullah.cse11@gmail.com>
2020-07-15 09:21:23 +0600

    add sidemenu using swift

---
+++ SideMenu + Programitically/SideMenu + Programitically/Drawer/DrawerProtocol.swift
@@ -0,0 +1,9 @@
+//
+//  DrawerProtocol.swift
+//  SideMenu + Programitically
+//
+//  Created by Muhammad Abdullah Al Mamun on 14/7/20.
+//  Copyright © 2020 Muhammad Abdullah Al Mamun. All rights reserved.
+//
+
+import Foundation
---
+++ SideMenu + Programitically/SideMenu + Programitically/ViewController/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SideMenu + Programitically
+//
+//  Created by Muhammad Abdullah Al Mamun on 14/7/20.
+//  Copyright © 2020 Muhammad Abdullah Al Mamun. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
