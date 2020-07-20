f75edf2d13a4b0705b582c1f34b6e27deeec0a6e
Raiyan <raiyan.sharif.1234@gmail.com>
2020-07-20 09:39:37 +0600

    project initilization

---
+++ CollectionViewTestApplication/CollectionViewTestApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViewTestApplication
+//
+//  Created by raiyan sharif on 20/7/20.
+//  Copyright © 2020 raiyansharif. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
